7f5aef5a269940a5b149c3a314e73fb53787fbb1
Lai Van Sam <samvasang1192011@gmail.com>
2026-08-03 23:11:41 +0700

    init db

--- configs/configs.py
+++ configs/configs.py
@@ -1,6 +1,13 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from pydantic import Field
 
-class Settings(BaseSettings): ...
+class Settings(BaseSettings):
+    SECRET_KEY: str = Field(..., alias="SECRET_KEY")
+    model_config = SettingsConfigDict(populate_by_name=True)
+    DEBUG: bool = Field(default=False, alias="DEBUG")
+    SESSION_COOKIE_SAMESITE = 'Lax'
+    SESSION_COOKIE_SECURE = DEBUG
+    SESSION_COOKIE_HTTPONLY = True
+
 
 Configs = Settings()
