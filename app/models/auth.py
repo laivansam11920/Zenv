cce307d549a2b5704650dbf9aa062aa4311a625c
Lai Van Sam <samvasang1192011@gmail.com>
2026-08-04 19:58:25 +0700

    add login

---
+++ app/models/auth.py
@@ -0,0 +1,19 @@
+from flask_login import UserMixin
+
+class User(UserMixin):
+    def __init__(self, username, password, user_id):
+        self.username: str = username
+        self.password: str = password
+        self.id: str = user_id
+
+    @staticmethod
+    def get_user(user_id: str) -> User | None:
+        from app.database import db
+
+        user = db.user.find_one({ "user_id": user_id }, { "_id": 0, "username": 1, "password": 1 })
+
+        if not user:
+            return None
+
+        return User(user_id=user.get("user_id"), username=user.get("username"), password=user.get("password"))
+
