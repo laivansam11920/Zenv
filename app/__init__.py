dc7900c2de42026c9bff9e09266816cb341200ba
Lai Van Sam <samvasang1192011@gmail.com>
2026-08-04 14:48:46 +0700

    del cors

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,8 @@
 from flask import Flask, render_template
 from configs import Configs
-from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
-    CORS(app, supports_credentials=True, origins="*") #<--
     app.secret_key = Configs.SECRET_KEY
     app.config.from_object(Configs)
     @app.route('/')
