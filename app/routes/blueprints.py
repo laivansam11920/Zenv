20370b0d6ab331d71c356c24a2c5de6ff73be5b0
Lai Van Sam <samvasang1192011@gmail.com>
2026-08-04 19:29:42 +0700

    add login

--- app/routes/blueprints.py
+++ app/routes/blueprints.py
@@ -2,5 +2,7 @@ from .routes import *
 from flask import Flask
 
 def register_routes(app: Flask):
-    app.register_blueprint(get_env_bp)
-    app.register_blueprint(login_bp)
+    app.register_blueprint(get_env_bp, url_prefix='/env')
+    app.register_blueprint(save_env_bp, url_prefix='/env')
+
+    app.register_blueprint(login_bp, url_prefix='/auth')
--- app/routes/routes.py
+++ app/routes/routes.py
@@ -1,10 +1,14 @@
 from flask import Blueprint
 
-get_env_bp: Blueprint = Blueprint('get_env', __name__)
-login_bp: Blueprint = Blueprint('login', __name__)
+get_env_bp: Blueprint = Blueprint('get_env_bp', __name__)
+save_env_bp = Blueprint('save_env_bp', __name__)
+login_bp: Blueprint = Blueprint('login_bp', __name__)
 
-@get_env_bp.post('/')
+@get_env_bp.get('/get_env')
 def get_env_route(): ...
+
+@save_env_bp.post('/save_env')
+def save_env_route(): ...
 
 @login_bp.post('/login')
 def login_route(): ...
