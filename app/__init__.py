15c31f9535b1f0e533e9b14e7358cbad33c8a160
Lai Van Sam <samvasang1192011@gmail.com>
2026-08-04 15:39:23 +0700

    add login route

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 from configs import Configs
 from app.routes import register_routes
 
-def create_app():
+def create_app() -> Flask:
     app = Flask(__name__)
     app.secret_key = Configs.SECRET_KEY
     app.config.from_object(Configs)
