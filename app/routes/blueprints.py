86dac6340654c640c69d4f38142ff6918e85ced0
Lai Van Sam <samvasang1192011@gmail.com>
2026-08-04 15:38:14 +0700

    add login route

--- app/routes/blueprints.py
+++ app/routes/blueprints.py
@@ -2,4 +2,5 @@ from .routes import *
 from flask import Flask
 
 def register_routes(app: Flask):
-    app.register_blueprint(get_env)
+    app.register_blueprint(get_env_bp)
+    app.register_blueprint(login_bp)
--- app/routes/routes.py
+++ app/routes/routes.py
@@ -1,6 +1,10 @@
 from flask import Blueprint
 
-get_env: Blueprint = Blueprint('get_env', __name__)
+get_env_bp: Blueprint = Blueprint('get_env', __name__)
+login_bp: Blueprint = Blueprint('login', __name__)
 
-@get_env.post('/')
+@get_env_bp.post('/')
 def get_env_route(): ...
+
+@login_bp.post('/login')
+def login_route(): ...
