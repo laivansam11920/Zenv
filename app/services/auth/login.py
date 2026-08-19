e103a63d86efd67d986a94915e7a7ec6c3af4c5d
Lai Van Sam <samvasang1192011@gmail.com>
2026-08-19 15:27:37 +0700

    add user model

--- app/services/auth/login.py
+++ app/services/auth/login.py
@@ -2,13 +2,14 @@ from app.database import db
 from app.extensions import ph
 from argon2.exceptions import VerifyMismatchError
 from threading import Thread
+from app.models.user import UserInfo
 
 
 class Login:
 
     @classmethod
-    def check(cls, email: str, password: str) -> bool:
-        user = db.users.find_one({"email": email}, {"_id": 0, "password": 1})
+    def check(cls, info: UserInfo) -> bool:
+        user = db.users.find_one({"email": info.email}, {"_id": 0, "password": 1})
 
         if not user or "password" not in user:
             return False
@@ -16,12 +17,12 @@ class Login:
         stored_hash: str = user["password"]
 
         try:
-            ph.verify(hash=stored_hash, password=password)
+            ph.verify(hash=stored_hash, password=info.password)
 
             if ph.check_needs_rehash(stored_hash):
                 Thread(
                     target=cls._update_password_hash,
-                    args=(email, ph.hash(password)),
+                    args=(info.email, ph.hash(info.password)),
                     daemon=True,
                 ).start()
 
